927cd077966829b420d3fc5fa4ee48ab73c9dee4
Jakob Knicley <knicleyjakob@gmail.com>
2022-03-17 11:46:39 -0400

    further style changes, added some actual content

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,15 +1,23 @@
-import styles from '../../styles/Home.module.css'
-import Link from 'next/link'
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
 
 const About = () => {
-  return ( 
+  return (
     <div className={styles.main}>
-      <div>
+      <div className={styles.center}>
         <h1>About Me</h1>
-        <p>Sorry, currently under development</p>
+        <p>
+          Full Stack Web Developer with a background in Production Operations.
+          Quick and eager learner who is technically proficient and can
+          understand and explain complex concepts. Skilled in critical thinking,
+          writing clean, scalable code, and organizing large problems into more
+          manageable steps. Understands the importance of a positive mindset,
+          taking initiative in order to find solutions, and always staying open
+          minded to direction.
+        </p>
       </div>
-    </div> 
+    </div>
   );
-}
- 
+};
+
 export default About;
--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,15 +1,24 @@
-import styles from '../../styles/Home.module.css'
-import Link from 'next/link'
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
 
 const Skills = () => {
-  return ( 
+  return (
     <div className={styles.main}>
-      <div>
+      <div className={styles.center}>
         <h1>Skills</h1>
-        <p>Sorry, currently under development</p>
+        <p>
+          Languages: HTML5, CSS3, JavaScript ES6+, C# 
+          <br></br>
+          Technologies: jQuery, Bootstrap, Node, Tailwind, Express, React,
+          Handlebars, MongoDB, MySQL, SQL Server, RESTful APIs, PWAs, Google
+          Cloud Computing
+          <br></br>
+          Concepts: Mobile-First Design, Data Structures, User Authentication,
+          Unit Testing, Debugging
+        </p>
       </div>
-    </div> 
+    </div>
   );
-}
- 
+};
+
 export default Skills;
